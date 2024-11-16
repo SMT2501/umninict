66f3ef1e02a1a1e016ac5a52a52bc27cc6d0c885
Samkele <sam@coding.com>
2024-11-16 11:18:48 +0200

    Umnini trust website commit

---
+++ app.js
@@ -0,0 +1,17 @@
+// app.js
+document.addEventListener("DOMContentLoaded", () => {
+    // Load header
+    fetch("header.html")
+        .then(response => response.text())
+        .then(data => {
+            document.querySelector("header").innerHTML = data;
+        });
+
+    // Load footer
+    fetch("footer.html")
+        .then(response => response.text())
+        .then(data => {
+            document.querySelector("footer").innerHTML = data;
+        });
+});
+
